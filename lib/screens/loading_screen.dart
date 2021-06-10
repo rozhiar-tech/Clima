0af9e3d44a0c37eb6b818cd6923d04dffa4feac3
Rozhiar <rozyar.313175000@uhd.edu.iq>
2021-06-10 23:30:24 +0300

    networking solved

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print(position);
+
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    getData();
+  }
+
+
+  void getLocation() async{
+    Location loc = Location();
+
+    await loc.getCurrentLocation();
+    print(loc.latitude);
+    print(loc.longitude);
+
+  }
+
+  // ignore: non_constant_identifier_names
+  void getData() async{
+
+
+    // var URL =Uri.https('api.openweathermap.org', '/data/2.5/weather?lat=35.4689&lon=45.4988&appid=2989ab784c2e79ac26850e63288b8680');
+
+    http.Response response = await http.get('');
+
+    if(response.statusCode == 200){
+      String data = response.body;
+    }else{
+      print(response.statusCode);
+    }
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+
     );
   }
 }
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -13,7 +13,7 @@ class _LocationScreenState extends State<LocationScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/location_background.jpg'),
+            image: AssetImage('images/get_location.jpg'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
                 Colors.white.withOpacity(0.8), BlendMode.dstATop),
